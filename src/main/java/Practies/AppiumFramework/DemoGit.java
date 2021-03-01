166595573c1ad880fd52d346c315beecd160795a
Krupa <Krupalamba72@gmail.com>
2021-03-02 00:28:42 +0530

    CommiteFromOther

---
+++ src/main/java/Practies/AppiumFramework/DemoGit.java
@@ -0,0 +1,11 @@
+package Practies.AppiumFramework;
+
+public class DemoGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is created to check the project under Git");
+	}
+
+}
