397ce251de8a555f85351bc75b8b898aa28d5df0
Krupa <Krupalamba72@gmail.com>
2021-03-02 22:50:31 +0530

    pullchangepush

---
+++ src/main/java/Practies/AppiumFramework/TestBranche2.java
@@ -0,0 +1,37 @@
+package Practies.AppiumFramework;
+
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class TestBranche2
+{
+	
+	 AndroidDriver<AndroidElement> driver;
+	public Utility(AndroidDriver<AndroidElement> drive)
+	{
+		this.driver=driver;
+	}
+	
+	public  void scoll(String element)
+	{
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\""+element +"\"))"); 
+	}
+
+	public void LongPress (WebElement element) throws InterruptedException
+	{
+		TouchAction t=new TouchAction(driver);	
+		t.longPress(longPressOptions().withElement(element(element)).withDuration(Duration.ofSeconds(5))).release().perform();
+		Thread.sleep(50);
+					
+	}
+
+	
+}
